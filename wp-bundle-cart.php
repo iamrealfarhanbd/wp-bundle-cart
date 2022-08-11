482cb316e9c3eea38e0019d53487f00231e9e8b0
iamrealfarhanbd <iamfarhan09@gmail.com>
2022-08-11 17:05:01 +0600

    capitalize

--- wp-bundle-cart.php
+++ wp-bundle-cart.php
@@ -13,20 +13,20 @@ if( ! defined( 'ABSPATH' ) ) : exit(); endif; // No direct access allowed.
 /**
 * Define Plugins Contants
 */
-define ( 'wpbc_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
-define ( 'wpbc_URL', trailingslashit( plugins_url( '/', __FILE__ ) ) );
+define ( 'WPBC_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+define ( 'WPBC_URL', trailingslashit( plugins_url( '/', __FILE__ ) ) );
 
 /**
  * Loading Necessary Scripts
  */
 add_action( 'admin_enqueue_scripts', 'load_scripts' );
 function load_scripts() {
-    wp_enqueue_script( 'wp-bundle-cart', wpbc_URL . 'dist/bundle.js', [ 'jquery', 'wp-element' ], wp_rand(), true );
+    wp_enqueue_script( 'wp-bundle-cart', WPBC_URL . 'dist/bundle.js', [ 'jquery', 'wp-element' ], wp_rand(), true );
     wp_localize_script( 'wp-bundle-cart', 'appLocalizer', [
         'apiUrl' => home_url( '/wp-json' ),
         'nonce' => wp_create_nonce( 'wp_rest' ),
     ] );
 }
 
-require_once wpbc_PATH . 'classes/class-create-admin-menu.php';
-require_once wpbc_PATH . 'classes/class-create-settings-routes.php';
+require_once WPBC_PATH . 'classes/class-create-admin-menu.php';
+require_once WPBC_PATH . 'classes/class-create-settings-routes.php';
